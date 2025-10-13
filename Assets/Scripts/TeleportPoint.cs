c6198670b78444fb9adc84884ede15fb82f6ecf4
kirrril <info@kirillukolov.com>
2025-10-13 13:31:17 +0200

    Commit_13_10_13h

--- Assets/Scripts/TeleportPoint.cs
+++ Assets/Scripts/TeleportPoint.cs
@@ -10,24 +10,23 @@ public class TeleportPoint : MonoBehaviour
     public void Teleport()
     {
         StartCoroutine(TeleportRoutine());
+
+        Debug.Log("Teleport called");
     }
 
     private IEnumerator TeleportRoutine()
     {    
         Vector3 start = xrOrigin.position;
-        // Quaternion startRot = xrOrigin.rotation;
         float elapsed = 0f;
 
         while (elapsed < transitionTime)
         {
             float t = elapsed / transitionTime;
             xrOrigin.position = Vector3.Lerp(start, destination.position, t);
-            // xrOrigin.rotation = Quaternion.Slerp(startRot, destination.rotation, t);
             elapsed += Time.deltaTime;
             yield return null;
         }
 
         xrOrigin.position = destination.position;
-        // xrOrigin.rotation = destination.rotation;
     }
 }
