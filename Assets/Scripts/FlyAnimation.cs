5d0e48ec8d9bd5d092da77319b6b12f1a7a727d6
kirrril <info@kirillukolov.com>
2025-10-18 22:38:49 +0200

    Commit_18_10_23h

--- Assets/Scripts/FlyAnimation.cs
+++ Assets/Scripts/FlyAnimation.cs
@@ -2,39 +2,50 @@ using UnityEngine;
 
 public class FlyAnimation : MonoBehaviour
 {
-    [SerializeField] private Transform mainCamera;
+    [SerializeField] private Transform xrOrigin;
     [SerializeField] private Animator animator;
     private Vector3 lastPosition;
     private float lastYaw;
 
     void Start()
     {
-        lastPosition = mainCamera.position;
-        lastYaw = mainCamera.eulerAngles.y;
+        lastPosition = xrOrigin.position;
+        lastYaw = xrOrigin.eulerAngles.y;
     }
 
     void Update()
     {
-        Vector3 delta = mainCamera.position - lastPosition;
+        Vector3 delta = xrOrigin.position - lastPosition;
         Vector3 flatDelta = new Vector3(delta.x, 0, delta.z);
+        Vector3 verticalDelta = new Vector3(0, delta.y, 0);
+        float up = verticalDelta.magnitude / Time.deltaTime;
         float forward = flatDelta.magnitude / Time.deltaTime;
 
-        float currentYaw = mainCamera.eulerAngles.y;
+        float currentYaw = xrOrigin.eulerAngles.y;
         float turn = Mathf.DeltaAngle(lastYaw, currentYaw) / Time.deltaTime;
 
-        animator.SetFloat("forward", forward);
-
-        if (forward < 0.01f)
+        if (up > 0.1)
         {
-            animator.SetFloat("turn", turn);
+            animator.SetFloat("up", up);
         }
         else
         {
-            animator.SetFloat("turn", 0f);
+            animator.SetFloat("up", 0);
+
+            animator.SetFloat("forward", forward);
+
+            if (forward < 0.1f && turn > 0.1f)
+            {
+                animator.SetFloat("turn", turn);
+            }
+            else
+            {
+                animator.SetFloat("turn", 0f);
+            }
+
         }
 
-
-        lastPosition = mainCamera.position;
+        lastPosition = xrOrigin.position;
         lastYaw = currentYaw;
     }
 }
