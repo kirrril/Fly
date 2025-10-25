04cedae8844783c8e39be45dc4ff848ed0e34d19
kirrril <info@kirillukolov.com>
2025-10-25 11:08:29 +0200

    Commit_25_10_11h

--- Assets/Scripts/BathroomLoader.cs
+++ Assets/Scripts/BathroomLoader.cs
@@ -5,8 +5,16 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class BathroomLoader : MonoBehaviour
 {
+    [SerializeField] private GameObject flat;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private FlyMover flyMover;
+    private static AsyncOperationHandle<GameObject> roomHandle;
+    private GameObject bathroomInstance;
+
+    void OnDestroy()
+    {
+        Addressables.Release(roomHandle);
+    }
 
     private async void OnTriggerEnter(Collider other)
     {
@@ -26,16 +34,17 @@ public class BathroomLoader : MonoBehaviour
 
     private async Task RoomLoader()
     {
-        var handle = Addressables.LoadAssetAsync<GameObject>("BathroomPrefab");
-        await handle.Task;
-        if (handle.Status == AsyncOperationStatus.Succeeded)
+        if (bathroomInstance != null) return;
+        
+        roomHandle = Addressables.InstantiateAsync("BathroomPrefab", spawnPoint.position, spawnPoint.rotation, flat.transform);
+        await roomHandle.Task;
+        if (roomHandle.Status == AsyncOperationStatus.Succeeded)
         {
-            Instantiate(handle.Result, spawnPoint.position, spawnPoint.rotation);
+            bathroomInstance = roomHandle.Result;
         }
         else
         {
             Debug.LogError("Échec chargement Bathroom");
         }
-        // Addressables.Release(handle);
     }
 }
